f30632f59669401bd7976f4584318355e27bafa1
Axel Daniel <axeelleguero@gmail.com>
2023-04-10 00:48:53 -0300

    Getters y setters agregados

--- src/ar/edu/unlam/pb1/finalAgosto/Contacto.java
+++ src/ar/edu/unlam/pb1/finalAgosto/Contacto.java
@@ -17,26 +17,105 @@ public class Contacto {
 	 * 
 	 */
 	private String nombre;
-	private int celular;
+	private Integer celular;
 	private String email;
 	private String direccion;
-	private int codigoPostal;
+	private Integer codigoPostal;
 	private String localidad;
-	//private enum Provincia;
-	private boolean esCliente;
-	private boolean deseaLlamada;
+	private Provincia provincia;
+	private Boolean esCliente;
+	private Boolean deseaLlamada;
 	
 	public void esEmailValido(String eMail) {
 		/*
 		 * Eval�a si un String determinado puede ser almacenado como E-MAIL.
 		 */
+//		for(int i=0;i<eMail.length();i++) {
+//			if(eMail.su == "@") {
+//				
+//			}
+//		}
 	}
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCelular() {
+		return celular;
+	}
+
+	public void setCelular(Integer celular) {
+		this.celular = celular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public Integer getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(Integer codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public Provincia getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(Provincia provincia) {
+		this.provincia = provincia;
+	}
+
+	public Boolean getEsCliente() {
+		return esCliente;
+	}
+
+	public void setEsCliente(Boolean esCliente) {
+		this.esCliente = esCliente;
+	}
+
+	public Boolean getDeseaLlamada() {
+		return deseaLlamada;
+	}
+
+	public void setDeseaLlamada(Boolean deseaLlamada) {
+		this.deseaLlamada = deseaLlamada;
+	}
+
 	public boolean registrarNuevaLlamada(Llamada nueva) {
 		/*
 		 * Registra una nueva llamada asociada al contacto actual.
 		 */
-		return false;
+		boolean registrada = false;
+		
+		return registrada;
 	}
 	
 	public String toString() {
--- src/ar/edu/unlam/pb1/finalAgosto/Empresa.java
+++ src/ar/edu/unlam/pb1/finalAgosto/Empresa.java
@@ -5,6 +5,8 @@ public class Empresa {
 	/*
 	 * Se deben incorporar los atributos necesarios.
 	 */
+	private String nombre;
+	private boolean agregado;
 	
 	public String getNombreEmpresa() {
 		/*
@@ -33,18 +35,18 @@ public class Empresa {
 	
 	private boolean elCodigoPostalEstaDentroDeLaZonaDeCobertura(int codigoPostal) {
 		/*
-		 * Determina si un código postal está dentro de la zona de cobertura
+		 * Determina si un cï¿½digo postal estï¿½ dentro de la zona de cobertura
 		 */
 		return false;
 	}
 	
 	public Contacto buscarCandidato() {
 		/*
-		 * Para determinar qué contacto el sistema debe mostrar, se debe realizar la siguiente búsqueda:
-		 * 1.	El contacto NO debe ser cliente aún.
-		 * 2.	El contacto desea ser llamado o al menos no informó lo contrario.
-		 * 3.	El código postal del contacto debe existir en las zonas de cobertura existente.
-		 * 4.	Del conjunto de contactos resultante se debe seleccionar aleatoriamente el próximo llamado.
+		 * Para determinar quï¿½ contacto el sistema debe mostrar, se debe realizar la siguiente bï¿½squeda:
+		 * 1.	El contacto NO debe ser cliente aï¿½n.
+		 * 2.	El contacto desea ser llamado o al menos no informï¿½ lo contrario.
+		 * 3.	El cï¿½digo postal del contacto debe existir en las zonas de cobertura existente.
+		 * 4.	Del conjunto de contactos resultante se debe seleccionar aleatoriamente el prï¿½ximo llamado.
 		 */
 
 		return null;
